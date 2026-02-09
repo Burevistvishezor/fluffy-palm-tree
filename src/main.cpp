ec8051b3cb25a04f18c2d4ca93b5b501ece51893
Banking simulations <ladimp11@gmail.com>
2026-02-09 15:09:53 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,1 +1,16 @@
+#include <iostream>
+#include "Bank.h"
+
+int main() {
+    Bank myBank(1000.0); // Создаем банк с начальным балансом 1000
+
+    std::cout << "Welcome to Fluffy Palm Tree Bank!\n";
+    myBank.displayBalance();
+
+    myBank.deposit(500);
+    myBank.withdraw(200);
+    myBank.displayBalance();
+
+    return 0;
+}
 
